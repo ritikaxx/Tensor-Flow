571bb3880b2f42b376ab4c2304e7bbcfa9f237c9
Ritika Singh <68536395+ritikaxx@users.noreply.github.com>
2021-05-10 18:54:33 +0530

    Add files via upload

---
+++ rnn-lstm-gru/rnn-lastm-gru.py
@@ -0,0 +1,63 @@
+import os
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.datasets import mnist
+
+#uploading the dataset
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+# x_train = x_train.reshape(-1, 784).astype("float32") / 255.0
+# x_test = x_test.reshape(-1, 784).astype("float32") / 255.0
+x_train = x_train.reshape([-1, 28, 28]).astype("float32") / 255.0
+x_test = x_test.reshape([-1, 28, 28]).astype("float32") / 255.0
+
+model = keras.Sequential()
+# specifying the output
+model.add(keras.Input(shape=(None, 28)))
+# simple rnn, 512 nodes, stacking each output from the rnn on top of each other
+model.add(layers.SimpleRNN(512, return_sequences=True, activation="relu"))
+# another layer, pass every timestamp
+model.add(layers.SimpleRNN(512, activation="relu"))
+# output layer, 10 output nodes
+model.add(layers.Dense(10))
+
+model = keras.Sequential()
+model.add(keras.Input(shape=(None, 28)))
+model.add(layers.SimpleRNN(256, return_sequences=True, activation="relu"))
+model.add(layers.SimpleRNN(256))
+model.add(layers.Dense(10))
+
+model = keras.Sequential()
+model.add(keras.Input(shape=(None, 28)))
+model.add(layers.GRU(256, return_sequences=True, activation="relu"))
+model.add(layers.GRU(256))
+model.add(layers.Dense(10))
+
+model = keras.Sequential()
+model.add(keras.Input(shape=(None, 28)))
+model.add(
+    layers.Bidirectional(layers.LSTM(256, return_sequences=True, activation="relu"))
+)
+model.add(layers.LSTM(256, name="lstm_layer2"))
+model.add(layers.Dense(10))
+
+model = keras.Sequential()
+model.add(keras.Input(shape=(None, 28)))
+model.add(
+    layers.Bidirectional(layers.LSTM(256, return_sequences=True, activation="relu"))
+)
+model.add(layers.Bidirectional(layers.LSTM(256, name="lstm_layer2")))
+model.add(layers.Dense(10))
+
+print(model.summary())
+model.compile(
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer=keras.optimizers.Adam(lr=0.001),
+    metrics=["accuracy"],
+)
+
+model.fit(x_train, y_train, batch_size=64, epochs=10, verbose=2)
+model.evaluate(x_test, y_test, batch_size=64, verbose=2)
